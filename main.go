67ba0a73d17443d107b73925c6e1b88badd38d3b
Anjalianju <anjaligopalakrishnan990@gmail.com>
2024-04-11 14:35:08 +0530

    Update main.go

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ var validConfig = []byte(`{
 func main(){
 	var password = "John" //type is string
 	var username string = "John" //type is string
-	secret_key := "DCJNSKDJNCDKJSNVKSJNKJSDNC"
+	secret_key := "a3b1c4d3e5f60789abcd2345ef678901"
 	h := md4.New()
 	data := "These pretzels are making me thirsty."
 	io.WriteString(h, data)
